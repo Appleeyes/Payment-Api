acc7ca907ea7179c0467a1f332bd644de6d8388b
Apple <appleeyespackage@gmail.com>
2023-10-30 02:39:50 +0100

    feat: added test for CustomerController

---
+++ Tests/A_ControllerTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace PaymentApiTests;
+
+use DI\Container;
+use Doctrine\ORM\EntityManager;
+use Mockery;
+use Monolog\Logger;
+use PaymentApi\Controller\CustomersController;
+use PaymentApi\Repository\CustomersRepository;
+use PaymentApi\Repository\CustomersRepositoryDoctrine;
+use PHPUnit\Framework\TestCase;
+
+class A_ControllerTest extends TestCase
+{
+    private Container $container;
+
+    protected function setUp(): void
+    {
+        $container = new Container();
+        $container->set(EntityManager::class, function (Container $c) {
+            return Mockery::mock('Doctrine\ORM\EntityManager');
+        });
+
+        $container->set(CustomersRepository::class, function (Container $c) {
+            $entitymanager = $c->get(EntityManager::class);
+            return new CustomersRepositoryDoctrine($entitymanager);
+        });
+
+        $container->set(Logger::class, function (Container $c) {
+            return Mockery::mock('Monolog\Logger');
+        });
+
+        $this->container = $container;
+    }
+    
+    public function testCreateInstanceOfCustomersController()
+    {
+        $repository = $this->container->get(CustomersRepository::class);
+        $abstractControllerObject = new CustomersController($this->container, $repository);
+        $this->assertInstanceOf('PaymentApi\Controller\CustomersController', $abstractControllerObject);
+    }
+}
---
+++ Tests/CustomersControllerTest.php
@@ -0,0 +1,278 @@
+<?php
+
+use DI\Container;
+use Doctrine\ORM\EntityManager;
+use Monolog\Logger;
+use PaymentApi\Controller\CustomersController;
+use PaymentApi\Model\Customers;
+use PaymentApi\Repository\CustomersRepository;
+use PaymentApi\Repository\CustomersRepositoryDoctrine;
+use PaymentApiTests\A_ControllerTest;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class CustomersControllerTest extends A_ControllerTest
+{
+    private $repository;
+    private $logger;
+    private $controller;
+    private $container;
+    
+    /**
+     * Method setUp
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $repository = $this->container->get(CustomersRepository::class);
+        $this->controller = new CustomersController($this->container, $repository);
+
+        $this->repository = $repository;
+        $this->logger = $this->container->get(Logger::class);
+    }
+    
+    /**
+     * Method testIndexAction
+     *
+     * @return void
+     */
+    public function testIndexAction()
+    {
+        $request = $this->createMock(ServerRequestInterface::class);
+        $response = $this->createMock(ResponseInterface::class);
+
+        $expectedData = [
+            [
+                "id" => 1,
+                "firstName" => "John",
+                "lastName" => "Doe",
+                "email" => "johndoe@example.com",
+                "isActive" => true,
+            ],
+
+            [
+                "id" => 2,
+                "firstName" => "Jane",
+                "lastName" => "Doe",
+                "email" => "janedoe@example.com",
+                "isActive" => false,
+            ]
+        ];
+        $this->repository->expects($this->once())
+            ->method('findAll')
+            ->willReturn([$expectedData]);
+
+        $this->logger->expects($this->once())
+            ->method('info');
+
+        $result = $this->controller->indexAction($request, $response);
+
+        $this->assertEquals(200, $result->getStatusCode());
+        $this->assertEquals(json_encode($expectedData), $result->getBody()->getContents());
+    }
+    
+    /**
+     * Method testCreateAction
+     *
+     * @return void
+     */
+    public function testCreateAction()
+    {
+        $requestData = [
+            "firstName" => "John",
+            "lastName" => "Doe",
+            "email" => "johndoe@example.com",
+            "isActive" => true,
+        ];
+
+        $request = $this->createMock(Request::class);
+        $request
+            ->method('getParsedBody')
+            ->willReturn($requestData);
+
+        $response = $this->createMock(Response::class);
+
+        $customer = new Customers();
+        $this->repository
+            ->expects($this->once())
+            ->method('store')
+            ->with($this->callback(function ($arg) use ($requestData) {
+                return $arg->getFirstName() === $requestData['firstName']
+                && $arg->getLastName() === $requestData['lastName']
+                && $arg->getEmail() === $requestData['email']
+                && $arg->getIsActive() === $requestData['isActive'];
+            }))
+            ->willReturn($customer);
+
+        $result = $this->controller->createAction($request, $response);
+
+        $this->assertEquals(200, $result->getStatusCode());
+
+        $expectedResponse = ['message' => 'Customer created successfully', 'customer_id' => $customer->getId()];
+        $this->assertEquals(json_encode($expectedResponse), $result->getBody());
+    }
+
+    
+    /**
+     * Method testRemoveAction
+     *
+     * @return void
+     */
+    public function testRemoveAction()
+    {
+        $customerId = 123;
+
+        $request = $this->createMock(Request::class);
+        $request
+            ->method('getAttribute')
+            ->with('id')
+            ->willReturn($customerId);
+
+        $response = $this->createMock(Response::class);
+
+        $customer = new Customers();
+        $this->repository
+            ->expects($this->once())
+            ->method('findById')
+            ->with($customerId)
+            ->willReturn($customer);
+
+        $this->repository
+            ->expects($this->once())
+            ->method('remove')
+            ->with($customer);
+
+        $result = $this->controller->removeAction($request, $response, ['id' => $customerId]);
+
+        $this->assertEquals(200, $result->getStatusCode());
+
+        $expectedResponse = ['message' => 'Customer Deleted'];
+        $this->assertEquals(json_encode($expectedResponse), $result->getBody());
+    }
+    
+    /**
+     * Method testDeactivateAction
+     *
+     * @return void
+     */
+    public function testDeactivateAction()
+    {
+        $customerId = 123;
+
+        $request = $this->createMock(Request::class);
+        $request
+            ->method('getAttribute')
+            ->with('id')
+            ->willReturn($customerId);
+
+        $response = $this->createMock(Response::class);
+
+        $customer = new Customers();
+        $this->repository
+            ->expects($this->once())
+            ->method('findById')
+            ->with($customerId)
+            ->willReturn($customer);
+
+        $this->repository
+            ->expects($this->once())
+            ->method('update')
+            ->with($customer);
+
+        $result = $this->controller->deactivateAction($request, $response, ['id' => $customerId]);
+
+        $this->assertEquals(200, $result->getStatusCode());
+
+        $expectedResponse = ['message' => 'Customer Deactivated'];
+        $this->assertEquals(json_encode($expectedResponse), $result->getBody());
+    }
+
+    
+    /**
+     * Method testReactivateAction
+     *
+     * @return void
+     */
+    public function testReactivateAction()
+    {
+        $customerId = 123;
+
+        $request = $this->createMock(Request::class);
+        $request
+            ->method('getAttribute')
+            ->with('id')
+            ->willReturn($customerId);
+
+        $response = $this->createMock(Response::class);
+
+        $customer = new Customers();
+        $this->repository
+            ->expects($this->once())
+            ->method('findById')
+            ->with($customerId)
+            ->willReturn($customer);
+
+        $this->repository
+            ->expects($this->once())
+            ->method('update')
+            ->with($customer);
+
+        $result = $this->controller->reactivateAction($request, $response, ['id' => $customerId]);
+
+        $this->assertEquals(200, $result->getStatusCode());
+
+        $expectedResponse = ['message' => 'Customer Reactivated'];
+        $this->assertEquals(json_encode($expectedResponse), $result->getBody());
+    }
+    
+    /**
+     * Method testUpdateAction
+     *
+     * @return void
+     */
+    public function testUpdateAction()
+    {
+        $customerId = 123;
+
+        $request = $this->createMock(Request::class);
+        $request
+            ->method('getAttribute')
+            ->with('id')
+            ->willReturn($customerId);
+
+        $updatedCustomerData = [
+            'firstName' => 'UpdatedFirstName',
+            'lastName' => 'UpdatedLastName',
+            'email' => 'updated@email.com',
+            'isActive' => true,
+        ];
+        $request
+            ->method('getParsedBody')
+            ->willReturn($updatedCustomerData);
+
+        $response = $this->createMock(Response::class);
+
+        $customer = new Customers();
+        $this->repository
+            ->expects($this->once())
+            ->method('findById')
+            ->with($customerId)
+            ->willReturn($customer);
+
+        $this->repository
+            ->expects($this->once())
+            ->method('update')
+            ->with($customer);
+
+        $result = $this->controller->updateAction($request, $response, ['id' => $customerId]);
+
+        $this->assertEquals(200, $result->getStatusCode());
+
+        $expectedResponse = ['message' => 'Customer Updated'];
+        $this->assertEquals(json_encode($expectedResponse), $result->getBody());
+    }
+}
